573432ec6a3d003051ea9f8a18fddb0b3e813b98
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-10-12 20:39:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,21 +4,35 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection)
+      for (let index = 0; index < newCollection.length; index++)
+        callback(newCollection[index])
+      return collection
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      if (!(collection instanceof Array))
+        collection = Object.values(collection)
+      const newArray = []
+      for (let index = 0; index < collection.length; index++)
+        newArray.push(callback(collection[index]))
+      return newArray
     },
 
-    reduce: function() {
-
+    reduce: function(collection = [], callback = () => {}, acc) {
+      let collection = collection.slice(0)
+      if (!acc) {
+        acc = collection[0]
+        collection = collection.slice(1)
+      }
+      let collectionLength = collection.length;
+      for (let i = 0; i < collectionLength; i++) {
+        acc = callback(acc, collection[i], collection)
+      }
+      return acc;
     },
 
-    functions: function() {
-
-    },
 
 
   }
